884ec73f214c02cad4c0a208517c2a5c40258191
patriknyfeldt <patriknyfeldt@hotmail.com>
2022-02-16 13:44:48 +0100

    added first draw function. still in progress

--- product-list/product-list.js
+++ product-list/product-list.js
@@ -1,25 +1,37 @@
+
+const queryString = new URLSearchParams(location.search);
+const qsCategory = queryString.get('category');
 const main = document.getElementById('productlist-main');
 let products = [];
 
-
-
+// const listProducts = (prodList) => {
+//     prodList.forEach(prod => {
+//         if(prod.category === qsCategory){
+//             main.innerHTML = `<article>
+//             <h2>${prod.name}</h2></article>`
+//         }
+//     })
+// }
 
 const getProducts = async () => {
-    const response = await fetch('/products.json');
+    const response = await fetch('../products.json');
     const data = await response.json();
     products = [...data.products];
-
+    
     products.forEach(product => {
-        let category = product.category;
+    if(product.category === qsCategory){
         let items = product.items;
-        
-       items.forEach(item =>{
-           console.log(item)
+        console.log(items)
+        items.forEach(item => {
+            console.log(item);
+            let h2 = document.createElement('h2');
+            h2.innerText = item.name;
+            main.appendChild(h2);
+        })
 
-           main.innerHTML = 
-           `<h1>${category}</h1>`
-       })
+    }
+
     })
-   
+                
 }
 getProducts();
