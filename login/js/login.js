ab626e8d6c039876531cece3b7c95d40f244d649
Richard Walton <richardwalton89@gmail.com>
2022-02-17 16:34:39 +0100

    login page
    added error message box to inform the user if something is wrong
    
    checks if user exists when typing email
    
    if found user, it makes sure the passowrd is correct.

--- login/js/login.js
+++ login/js/login.js
@@ -1,6 +1,7 @@
 const emailField = document.getElementById("email-field");
 const passwordField = document.getElementById("password-field");
 const activeUserDisplay = document.getElementById("active-user-display");
+const errorMessageDisplay = document.getElementById("error-message");
 
 let userList = [];
 const userData = JSON.parse(localStorage.getItem("userList"))
@@ -13,31 +14,38 @@ document.getElementById("login-button").addEventListener("click", e =>{
     e.preventDefault();
     if(emailField.value === "" || passwordField.value === ""){
         passwordField.value = "";
-        console.log("required fields are empty");
+        printErrorMessage("Fyll in de tomma fälten.");
         return;
     }
     
     if(userExists(emailField.value)){
-        localStorage.setItem("user", JSON.stringify(getUser(emailField.value)));
+        const user = getUser(emailField.value);
+        if(user.password !== passwordField.value){
+            printErrorMessage("Fel lösenord.");
+            return;
+        }
+        localStorage.setItem("user", JSON.stringify(user));
         emailField.value = "";
         passwordField.value = "";
         location.reload();
     }
     else{
-        activeUserDisplay.innerText = `User doesn't exist`;
+        printErrorMessage("Finns ingen användare med den eposten.")
         passwordField.value = "";
     }
 })
 
 const getUser = (email) => {
-    return userList.find(user =>{
-        if(user.email === email){
-            console.log("found user! ");
-            return user;
-        }
-    })
+    return userList.find(user => user.email === email);
 }
 
 const userExists = (email) =>{
     return userList.find(user => user.email === email)? true : false;
+}
+
+function printErrorMessage(text){
+    if(errorMessageDisplay.parentElement.classList.contains("hidden")){
+        errorMessageDisplay.parentElement.classList.remove("hidden");
+    }
+    errorMessageDisplay.innerText = text;
 }
