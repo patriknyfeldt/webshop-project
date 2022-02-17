6255a81d26fa8c5c91aeabdbf7293f5585abde63
Nennae <sezenerdal@outlook.com>
2022-02-17 17:46:34 +0100

    layout in progress

--- js/product.js
+++ js/product.js
@@ -1,7 +1,20 @@
-console.log("Hoohoo");
 
 const productContainer = document.querySelector('product-info');
+// Variable name examples: productItem, info , (content), (item)
+let content = [];
 
-const renderProduct = () => {
-      
+const renderProduct = (item) => {
+productContainer.innerHTML = `
+      <div class="flex-container" id="${item.id}">
+            <div class="image-wrapper">
+            <img class="image" src="${item.image}">
+            </div>
+      <div class="info-wrapper">
+            <h2>${item.name}</h2>
+                  <p>Description: ${item.description}<p/>
+                  <p>Price: ${item.price}<p/>
+      <button id="add-btn-${item.id}">Lägg till i varukorg</button>
+            </div>
+      </div>`;
 }
+console.log(renderProduct(item));
