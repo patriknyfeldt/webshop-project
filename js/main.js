159cb37198fee86dd816ec4e7b7cc7533b96ec81
Richard Walton <richardwalton89@gmail.com>
2022-02-17 15:14:35 +0100

    login
    added function for each page to remember logged in user
    added main.js file for each page
    
    when logging in, the users name will be displayed in the upper right corner, instead of Log in link

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let activeUser;
+
+//when page loads, sets active user info to DOM
+function userInit(){
+    userData = JSON.parse(localStorage.getItem("user"));
+    
+    //check if it's an empty object or if we fetched some data
+    if(userData){
+        activeUser = userData;
+        const userNameDisplay = document.getElementById("user-name");
+        const loginDisplay = document.getElementById("log-in-link");
+        if(userNameDisplay){
+            userNameDisplay.classList.remove("hidden");
+            userNameDisplay.innerText = `Welcome ${activeUser.forename}!`;
+
+        }
+        if(loginDisplay){
+            loginDisplay.classList.add("hidden");
+        }
+    }
+}
+
+userInit();
--- login/js/login.js
+++ login/js/login.js
@@ -1,17 +1,12 @@
 const emailField = document.getElementById("email-field");
 const passwordField = document.getElementById("password-field");
 const activeUserDisplay = document.getElementById("active-user-display");
-let activeUser = JSON.parse(localStorage.getItem("user"));
 
 let userList = [];
 const userData = JSON.parse(localStorage.getItem("userList"))
 
 if(userData){
     userList = [...userData]
-}
-
-if(activeUser){
-    activeUserDisplay.innerText = `Welcome ${activeUser.email}!`;
 }
 
 document.getElementById("login-button").addEventListener("click", e =>{
@@ -21,18 +16,12 @@ document.getElementById("login-button").addEventListener("click", e =>{
         console.log("required fields are empty");
         return;
     }
-    const loginInfo = {
-        email: emailField.value,
-        password: passwordField.value
-    }
-
     
-    if(isExistingUser(loginInfo)){
-        localStorage.setItem("user", JSON.stringify(loginInfo));
-        activeUser = loginInfo;
-        activeUserDisplay.innerText = `Welcome ${activeUser.email}!`;
+    if(userExists(emailField.value)){
+        localStorage.setItem("user", JSON.stringify(getUser(emailField.value)));
         emailField.value = "";
         passwordField.value = "";
+        location.reload();
     }
     else{
         activeUserDisplay.innerText = `User doesn't exist`;
@@ -40,15 +29,15 @@ document.getElementById("login-button").addEventListener("click", e =>{
     }
 })
 
-const isExistingUser = (loginDetail) => {
-    console.log(`loginDetail: ${loginDetail.email} ${loginDetail.password}`);
-    let flag = false;
-    userList.forEach(user =>{
-        console.log(`user: ${user.email} ${user.password}`);
-        if(user.email === loginDetail.email && user.password === loginDetail.password){
-            console.log("found user!");
-            flag = true;
+const getUser = (email) => {
+    return userList.find(user =>{
+        if(user.email === email){
+            console.log("found user! ");
+            return user;
         }
     })
-    return flag;
+}
+
+const userExists = (email) =>{
+    return userList.find(user => user.email === email)? true : false;
 }
