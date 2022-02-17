769929ae6b4613a2421e52d741c1371b62233944
Andre Odmark <andre.odmark@gmail.com>
2022-02-17 15:03:43 +0100

    function sol lagrar varor i array till kundvagnen

--- product-list/product-list.js
+++ product-list/product-list.js
@@ -2,14 +2,16 @@
 const queryString = new URLSearchParams(location.search);
 const qsCategory = queryString.get('category');
 const productList = document.getElementById('product-list');
+let buttons;
+let articles;
 let products = [];
 
 
 
 const drawProduct = (item) =>
-    
+
     `<section class="products-wrapper">
-    <articel class="product-article "id=${item.id}>
+    <articel class="product-article "id=article-${item.id}>
     <h2>${item.name}</h2>
     <div class="article-content-wrapper">
     <div class="article-left-wrapper">
@@ -20,24 +22,72 @@ const drawProduct = (item) =>
     <br>
     <p>Price: ${item.price}<p/>
     <br>
-    <button id="add-btn-${item.id}">lägg till i varukorgen</button>
+    <button class="add-to-cart" id="addbtn-${item.id}">lägg till i varukorgen</button>
     </div>
     </div>
     </article>
     </section>`;
+    
 
 
 const getProducts = async () => {
     const response = await fetch('../products.json');
     const data = await response.json();
     products = [...data.products];
-    
+
     products.forEach(product => {
     if(product.category === qsCategory){
         let items = product.items;
-            productList.innerHTML = items.map(drawProduct).join('');
-        console.log(productList.innerHTML);
+        productList.innerHTML = items.map(drawProduct).join('');
+    
+        
+        //test
+        buttons = productList.querySelectorAll(".add-to-cart");
+        articles = productList.querySelectorAll(".product-article");
+        shoppingCart = document.getElementById("shopping-cart-list");
+        let articlesForChart = [];
+        let artId;
+
+        buttons.forEach(button => {
+            
+            button.addEventListener('click', (e) => {
+                    let productID = e.target.id.slice(7);
+
+                    articles.forEach(article =>{
+                     artId = article.id.slice(8);
+
+                     if(artId === productID){
+                         articlesForChart.push(article);
+                        console.log(articlesForChart);
+                        localStorage.setItem("basket", JSON.stringify(articlesForChart))
+                   }   
+                                        
+                 })
+                
+                
+            })
+
+        })
+
+         
+            
+           
+            
+            
+            
+            
+        
+            
+                
+            
     }
     })
+   
 }
+
 getProducts();
+
+/* articles.forEach((article) => {
+    console.log(article);
+  }) */
+
