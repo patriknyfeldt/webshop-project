31c14a4fdc3d5324c66ce010d6fce68c1766f691
Andre Odmark <andre.odmark@gmail.com>
2022-02-18 11:51:59 +0100

    added counter input shopping baskes

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -2,7 +2,13 @@ const chosenProductCart = document.getElementById("chosen-product-cart");
 
 let savedProducts = JSON.parse(localStorage.getItem("basket"));
 let savedProductList = savedProducts.list;
-console.log(savedProductList);
+
+/* let add = document.getElementsByClassName("add-more");
+let sub = document.getElementsByClassName("add-less");
+let numbItems = document.getElementsByClassName("numb-items"); */
+/* console.log(savedProductList); */
+
+
 
 
     savedProductList.forEach(product => {
@@ -16,18 +22,21 @@ console.log(savedProductList);
     <p>Description: ${product.description}</p>
     </div>
         
-    <div class="shopping-cart-counter">
+    <label for="product-count">antal</label>
+    <input type="number" id="product-count" min="1">
+    <b>price: ${product.price}</b>
+    </div>`;
+
+    console.log(product.price);
+    });
+    
+/*     <div class="shopping-cart-counter">
     <div>
     <p>antal</p>
-    <p>1</p>
+    <p class="numb-items">1</p>
     </div>
     <div class="shopping-cart-counter-btn">
     <div class="add-more"></div>
     <div class="line-between"></div>
     <div class="add-less"></div>
-    </div>
-
-    <b>price: ${product.price}</b>
-    </div>`;
-        
-    });
+    </div> */
