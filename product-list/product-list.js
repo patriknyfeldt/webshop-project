8a1234cf87e381bd98c278b68fbe42dc5e96ab78
Richard Walton <richardwalton89@gmail.com>
2022-02-21 11:25:28 +0100

    Product page
    You can add the product to the cart via the buy button

--- product-list/product-list.js
+++ product-list/product-list.js
@@ -53,52 +53,51 @@ const drawProduct = (item) =>
 
         // TILLHÖR VARUKORGS FUNKTIONEN
 
-        addToShoppingCart();
-        // buttons = productList.querySelectorAll(".add-to-cart");
-        // articles = productList.querySelectorAll(".product-article");
-        // // Behövs här eller i shopping-cart.js?
-        // shoppingCart = document.getElementById("shopping-cart-list");
+        buttons = productList.querySelectorAll(".add-to-cart");
+        articles = productList.querySelectorAll(".product-article");
+        // Behövs här eller i shopping-cart.js?
+        shoppingCart = document.getElementById("shopping-cart-list");
         
-        // buttons.forEach(button => {
+        buttons.forEach(button => {
             
-        //     button.addEventListener('click', (e) => {
-        //         let productID = e.target.id.slice(7);
+            button.addEventListener('click', (e) => {
+                let productID = e.target.id.slice(7);
                 
 
-        //         const chosenProduct = listedProducts.find(product => product.id === productID);
+                const chosenProduct = listedProducts.find(product => product.id === productID);
                
                 
-        //         /* console.log(chosenProduct); */
+                /* console.log(chosenProduct); */
 
-        //         let articlesForChartObject = JSON.parse(localStorage.getItem("basket"));
-        //         let articlesForChart = [];
-        //         if(articlesForChartObject){
-        //             articlesForChart = articlesForChartObject.list;
-        //         }
+                let articlesForChartObject = JSON.parse(localStorage.getItem("basket"));
+                let articlesForChart = [];
+                if(articlesForChartObject){
+                    articlesForChart = articlesForChartObject.list;
+                }
 
-        //         const existingProduct = articlesForChart.find(a => a.article.id === chosenProduct.id)
-        //         if(existingProduct){
-        //             existingProduct.quantity++
-        //         }
-        //         else if(!existingProduct){
-        //             const articleObj = {
-        //                 quantity: 1,
-        //                 article: chosenProduct
-        //             }
-        //             articlesForChart.push(articleObj);
-        //         }
+                const existingProduct = articlesForChart.find(a => a.article.id === chosenProduct.id)
+                if(existingProduct){
+                    existingProduct.quantity++
+                }
+                else if(!existingProduct){
+                    const articleObj = {
+                        quantity: 1,
+                        article: chosenProduct
+                    }
+                    articlesForChart.push(articleObj);
+                }
 
 
-        //             const basketList = {
-        //             list: articlesForChart
-        //         }
+                    const basketList = {
+                    list: articlesForChart
+                }
                 
-        //         localStorage.setItem("basket", JSON.stringify(basketList));
+                localStorage.setItem("basket", JSON.stringify(basketList));
                 
                 
-        //     })
+            })
 
-        // }) 
+        }) 
     
 
 }
--- product/product.js
+++ product/product.js
@@ -2,9 +2,8 @@
 const queryString = new URLSearchParams(location.search);
 const qsArticles = queryString.get('article');
 const productContainer = document.getElementById('product-info');
-let idButtton = "";
+let product;
 
-console.log(qsArticles);
 
 let products = []; // Array to store the data in products.json
 let allItems = []; // Array to store products that are copied ^
@@ -22,62 +21,60 @@ const getArticles = async () => {
             })
             
       })
-      console.log(allItems);
-      console.log(qsArticles);
 
       const item = allItems.find(item => item.id === qsArticles);
-      console.log(item);
+      product = item;
 
       if(item){
                   
             productContainer.innerHTML += 
                   `<articel class="product-article "id=article-${item.id}>
-                  <div class="flex-container">
-                  <div class="image-container">
-                  <a href="../product/product.html?article=${item.id}"><img class="product-img" src=${item.image} alt= ${item.alt}></img></a>
-                  </div>
-                  <div class="info-container">
-                  <h2>${item.name}</h2>
-                  <p>Beskrivning: ${item.description}<p/>
-                  <br>
-                  <p>Pris: ${item.price}<p/>
-                  <br>
-                  <button class="add-to-cart" id="addbtn-${item.id}">Köp<button>
-                  </div>
-                  </div>
+                        <div class="flex-container">
+                              <div class="image-container">
+                                    <a href="../product/product.html?article=${item.id}"><img class="product-img" src=${item.image} alt= ${item.alt}></img></a>
+                              </div>
+                        <div class="info-container">
+                              <h2>${item.name}</h2>
+                              <p>Beskrivning: ${item.description}<p/>
+                              <br>
+                              <p>Pris: ${item.price}<p/>
+                              <br>
+                              <button class="add-to-cart" id="addbtn-${item.id}">Köp<button>
+                              </div>
+                        </div>
                   </article>`;  
       }
 
 console.log(document.getElementById(`addbtn-${item.id}`));
 document.getElementById(`addbtn-${item.id}`).addEventListener('click', () => {
       console.log("add to basket");
-            // const chosenProduct = listedProducts.find(product => product.id === productID);
-            /* console.log(chosenProduct); */
 
-            let articlesForChartObject = JSON.parse(localStorage.getItem("basket"));
-            let articlesForChart = [];
-            if(articlesForChartObject){
-                  articlesForChart = articlesForChartObject.list;
-            }
-
-            const existingProduct = articlesForChart.find(a => a.item.id === chosenProduct.id)
-            if(existingProduct){
-                  existingProduct.quantity++
-            }
-            else if(!existingProduct){
-                  const articleObj = {
+      let articlesForChartObject = JSON.parse(localStorage.getItem("basket"));
+      let articlesForChart = [];
+      if(articlesForChartObject){
+            articlesForChart = articlesForChartObject.list;
+      }
+      
+      console.log(articlesForChart)
+      const existingProduct = articlesForChart.find(a => a.article.id === product.id)
+      if(existingProduct){
+            existingProduct.quantity++
+      }
+      else if(!existingProduct){
+            const articleObj = {
                   quantity: 1,
-                  article: chosenProduct
-                  }
-                        articlesForChart.push(articleObj);
+                  article: product
             }
-                  const basketList = {
-                  list: articlesForChart
-            }
-            
-            localStorage.setItem("basket", JSON.stringify(basketList));
-      })
-      addToShoppingCart();
+            articlesForChart.push(articleObj);
+      }
+      const basketList = {
+            list: articlesForChart
+      }
+      console.log(basketList)
+      
+      localStorage.setItem("basket", JSON.stringify(basketList));
+})
+// addToShoppingCart();
       
 }
 getArticles();
