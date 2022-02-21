d1396d75b61273be3160c0de8267f6fe3e18d90f
Richard Walton <richardwalton89@gmail.com>
2022-02-21 10:31:30 +0100

    order formula
    all fields are filled with user data if user is logged in

--- order/js/order.js
+++ order/js/order.js
@@ -1,9 +1,14 @@
 import {activeUser} from "../../js/main.js"
 
 if(activeUser){
-    const fieldInputs = document.querySelectorAll('input.input-field');
+    const fieldInputs = [...document.querySelectorAll('input.input-field')];
     fieldInputs.forEach(input =>{
+        console.log(input)
         const variableName = input.id.slice(15);
-        input.value = activeUser[variableName]
+        if(activeUser.hasOwnProperty(variableName)){
+            input.value = activeUser[variableName];
+        }
     })
 }
+
+document.getElementById("")
