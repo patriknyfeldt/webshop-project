350763a437ea420c01b13e927746cabfc6f7c032
Richard Walton <richardwalton89@gmail.com>
2022-02-23 11:34:05 +0100

    error message functionality added
    checks if any required fields are missing when updating
    
    checks if password fields are matching when updating

--- profile/js/profilescript.js
+++ profile/js/profilescript.js
@@ -1,7 +1,7 @@
 import { activeUser } from "../../js/main.js";
 
 const inputFields = document.querySelectorAll(".input-field")
-
+const errorMessage = document.getElementById("error-message");
 
 function fillInform(){
     if(activeUser){
@@ -17,9 +17,27 @@ function fillInform(){
 
 fillInform();
 
+function printErrorMessage(text){
+    console.log(errorMessage);
+    if(errorMessage.parentElement.classList.contains("hidden")){
+        errorMessage.parentElement.classList.remove("hidden");
+    }
+    errorMessage.innerText = text;
+}
+
 document.getElementById("update-btn").addEventListener("click", (e)=>{
-    console.log("update")
     e.preventDefault();
+    console.log("update")
+
+    if(Array.from(inputFields).some(input => input.required && input.validity.valueMissing)){
+        printErrorMessage("Fyll in de tomma fälten med stjärnor(*).");
+        return;
+    }
+    else if(document.getElementById("password-field").value !== document.getElementById("password-verify-field").value){
+        printErrorMessage("Lösenord är inte matchade.")
+        return;
+    }
+
     inputFields.forEach(field =>{
         const fieldName = field.id.split("-")[0]
         activeUser[fieldName] = field.value;
