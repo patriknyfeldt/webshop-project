d3237d6101ff9aa5b04fdebf81f76ccea30b952b
Richard Walton <richardwalton89@gmail.com>
2022-02-23 11:17:05 +0100

    profile
    you can now update your profile info
    
    users can log out
    
    put email field on top of formula and also disabled it, because emails must be unique

--- profile/js/profilescript.js
+++ profile/js/profilescript.js
@@ -2,13 +2,33 @@ import { activeUser } from "../../js/main.js";
 
 const inputFields = document.querySelectorAll(".input-field")
 
-document.getElementById("update-btn").addEventListener("click", (e)=>{
-    e.preventDefault();
-    console.log("update");
-})
 
 function fillInform(){
-    console.log(inputFields);
+    if(activeUser){
+        inputFields.forEach(field =>{
+            field.value = activeUser[field.id.split("-")[0]];
+        })
+        
+    }
+    else{
+        console.log("user not logged in");
+    }
 }
 
 fillInform();
+
+document.getElementById("update-btn").addEventListener("click", (e)=>{
+    console.log("update")
+    e.preventDefault();
+    inputFields.forEach(field =>{
+        const fieldName = field.id.split("-")[0]
+        activeUser[fieldName] = field.value;
+    })
+    localStorage.setItem("user", JSON.stringify(activeUser))
+    location.reload();
+})
+
+document.getElementById("logout-btn").addEventListener("click", ()=>{
+    localStorage.removeItem("user");
+    window.location.pathname = ('../index.html');
+})
